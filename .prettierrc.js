ca79847275dd28a02641455bec93ebf6089ab890
Diogo <43014908+okaydiogo@users.noreply.github.com>
2022-02-10 21:05:27 +0000

    node, vercel and prettier stuff

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  quoteProps: 'consistent',
+  trailingComma: 'all',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  semi: true,
+  arrowParens: 'always',
+  printWidth: 110,
+  importOrderSeparation: true,
+};
--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  tailwindConfig: './tailwind.config.js',
-  quoteProps: 'consistent',
-  trailingComma: 'all',
-  singleQuote: true,
-  jsxSingleQuote: true,
-};
